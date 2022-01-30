4882334b84d22ded3f0b03cb3a34376aede83fea
Oscar <oscargm28@hotmail.com>
2022-01-30 17:09:34 +0100

    starter con rutas nested preparado

--- 02-lazyload-v6/src/routes/routes.ts
+++ 02-lazyload-v6/src/routes/routes.ts
@@ -1,44 +1,31 @@
 import { lazy, LazyExoticComponent } from "react";
-// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 
-/** 
- * Fijate que es Component y no component
- * Fijate que path es lo mismo que to pero sin el /
- * Y dado que estoy en TS mejor definir una interface
- */
+
 type JSXComponent = () => JSX.Element;
 
 interface Routes {
   to: string;
   path: string;
-  // Component: LazyExoticComponent<() => JSX.Element> | (() => JSX.Element);
-  /* realmeente puedo refactorizar un poco lo de arriba ya que comparten retornar un fc */
   Component: LazyExoticComponent<JSXComponent> | JSXComponent;
   name: string;
 }
 
-const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'));
+const LazyLayout = lazy( () => import(/*webpackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout') );
+
 
 export const routes: Routes[] = [
   { 
-    to: '/lazy1',
-    path:'lazy1',
-    Component: Lazy1,
-    name: 'Lazy-1',
+    path:'/lazyload/*',
+    to: '/lazyload', //no usar '/' al final para que lo detecte props.isActive,si pongo /lazyload/ entonces no lo detecta
+    Component: LazyLayout,
+    name: 'LazyLayout-Dashboard',
   },
   { 
-    to: '/lazy2',
-    path:'lazy2',
-    Component: Lazy2,
-    name: 'Lazy-2',
-  },
-  { 
-    to: '/lazy3',
-    path:'lazy3',
-    Component: Lazy3,
-    name: 'Lazy-3',
+    path:'no-lazy',
+    to: '/no-lazy',
+    Component: NoLazy,
+    name: 'No Lazy',
   },
 ]
