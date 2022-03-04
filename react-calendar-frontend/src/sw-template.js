104cfbb87a391d827405b2deef5a786b75e1446f
Oscar <oscargm28@hotmail.com>
2022-03-04 20:26:34 +0100

    workbox-cli added

---
+++ react-calendar-frontend/src/sw-template.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
+);
+// eslint-disable-next-line no-restricted-globals
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
